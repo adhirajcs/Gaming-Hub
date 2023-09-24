49bc5e8efd43009f0cb028156e78708111ad626b
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2023-09-24 15:20:47 +0000

    Implemented home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component,  OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse, Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +9,32 @@ import { Component,  OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+openGameDetails(arg0: any) {
+throw new Error('Method not implemented.');
+}
   public sort: string | undefined;
+  public games: Array<Game> | undefined;
 
-  constructor() {}
+  constructor(
+    private httpService: HttpService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
-  ngOnInit(): void {
-    
+   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.httpService
+      .getGameList(sort, search).subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results;
+        console.log(gameList);
+      });
   }
 }
