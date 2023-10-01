7bf924bc12730314edb6c0db8c25b8021ca3648c
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2023-10-01 16:29:16 +0000

    Implemented details component

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment as env} from 'src/environments/environment.development';
+import { Observable, forkJoin, map } from 'rxjs';
+import { environment as env } from 'src/environments/environment.development';
 import { APIResponse, Game } from '../models';
+import { ObserversModule } from '@angular/cdk/observers';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,40 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  getGameList(ordering:string, search?: string): Observable<APIResponse<Game>> {
-   let params = new HttpParams().set('ordering', ordering);
-   
-   if(search) {
-     let params = new HttpParams().set('ordering', ordering).set('search',search);
-   }
+  getGameList(ordering: string, search?: string): Observable<APIResponse<Game>> {
+    let params = new HttpParams().set('ordering', ordering);
 
-   return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
-    params: params,
-  });
+    if (search) {
+      let params = new HttpParams().set('ordering', ordering).set('search', search);
+    }
+
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
+      params: params,
+    });
+  }
+
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/$(id)`);
+    const gameTrailersRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/screenshots`
+    );
+
+    return forkJoin({
+      gameInfoRequest,
+      gameScreenshotsRequest,
+      gameTrailersRequest,
+    }).pipe(
+      map((resp: any) => {
+        return {
+          ...resp['gameInfoRequest'],
+          screenshots: resp['gameScreenshotsRequest']?.results,
+          trailers: resp['gameTrailersRequest']?.results,
+        };
+      })
+    );
+
   }
 }
